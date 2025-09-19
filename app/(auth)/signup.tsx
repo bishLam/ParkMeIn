e3d70e111a3d3b052e3c107e10d1a7cfc48639df
minaa <minaalsyeda@gmail.com>
2025-09-19 12:21:17 +1000

    Implemented Validation Login for Sign Up

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useContext, useEffect, useState } from "react";
+import { isValidEmail, isValidPassword } from "@/utils/helper";
 import { StyleSheet, Text, View } from 'react-native'
 
 const SignupPage = () => {
@@ -9,6 +11,25 @@ const SignupPage = () => {
   )
 }
 
-export default SignupPage
+// Minaal TO-DO List:
+// - Click "Sign Up" -> Navigate to Home Page signed in
+// - Click "Already heave an account? Sign In" -> Navigate to Sign In
+
+export default function SignUp(){
+    // State Variables
+    const [email, setEmail] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [password, setPassword] = useState('');
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false); // Will use this  once a toggle button has been added
+    const [acceptedTCs, setAcceptedTCs] = useState(false); // For the T&Cs/Privacy Policy checkbox
+    const [auth, setAuth] = useState(null);
+
+    // Validate Email and Password
+    const validEmail = isValidEmail(email);
+    const validPassword = isValidPassword(password);
+
+    const isFormValid = validEmail && validPassword && fullName.length > 0 && acceptedTCs;       
+
+}
 
 const styles = StyleSheet.create({})
